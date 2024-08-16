c0b6b2518c5f7af0b97a6b6aba6589f94b5659dc
iku <121695593+notiku@users.noreply.github.com>
2024-08-16 20:38:18 +0200

    add tooltips

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -113,10 +113,10 @@
         <div class="subtitle">{subtitle}</div>
     </div>
     <div class="socials">
-        <a target="_blank" href={discordLink}><img src="discord.svg" class="discord" alt=""></a>
-        <a target="_blank" href={githubLink}><img src="github.svg" class="github" alt=""></a>
+        <a target="_blank" href={discordLink} tooltip="schizo"><img src="discord.svg" class="discord" alt=""></a>
+        <a target="_blank" href={githubLink} tooltip="@notiku"><img src="github.svg" class="github" alt=""></a>
         <!-- <a target="_blank" href={twitterLink}><img src="twitter.svg" class="twitter" alt=""></a> -->
-        <a target="_blank" href={`mailto:${email}`}><img src="mail.svg" class="mail" alt=""></a>
+        <a target="_blank" href={`mailto:${email}`} tooltip="contact@iku.pet"><img src="mail.svg" class="mail" alt=""></a>
     </div>
     <div class="links">
         <a target="_blank" href={discordBotLink}>Fraud Discord Bot</a>
